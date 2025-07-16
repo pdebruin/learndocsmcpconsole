bd42a35b72a0c33f42c8efa61cc3f356a3264560
Pieter de Bruin <pdebruin@hotmail.com>
2025-07-16 13:59:05 +0200

    local

--- src/Githubmodel.cs
+++ src/Githubmodel.cs
@@ -6,35 +6,34 @@ class Githubmodel
 {
     public static async Task Main(string[] args)
     {
-
-// TODO: merge with https://github.com/modelcontextprotocol/csharp-sdk/blob/main/samples/ChatWithTools/Program.cs
+        // TODO: merge with https://github.com/modelcontextprotocol/csharp-sdk/blob/main/samples/ChatWithTools/Program.cs
 
         var endpoint = "https://models.github.ai/inference";
-var credential = System.Environment.GetEnvironmentVariable("GITHUB_TOKEN");
-var model = "openai/gpt-4o";
+        var credential = System.Environment.GetEnvironmentVariable("GITHUB_TOKEN");
+        var model = "openai/gpt-4o";
 
-var openAIOptions = new OpenAIClientOptions()
-{
-    Endpoint = new Uri(endpoint)
+        var openAIOptions = new OpenAIClientOptions()
+        {
+            Endpoint = new Uri(endpoint)
 
-};
+        };
 
-var client = new ChatClient(model, new ApiKeyCredential(credential), openAIOptions);
+        var client = new ChatClient(model, new ApiKeyCredential(credential), openAIOptions);
 
-List<ChatMessage> messages = new List<ChatMessage>()
-{
-    new SystemChatMessage("You are a helpful assistant."),
-    new UserChatMessage("What is the capital of France?"),
-};
+        List<ChatMessage> messages = new List<ChatMessage>()
+        {
+            new SystemChatMessage("You are a helpful assistant."),
+            new UserChatMessage("What is the capital of France?"),
+        };
 
-var requestOptions = new ChatCompletionOptions()
-{
-    Temperature = 1.0f,
-    TopP = 1.0f,
-    MaxOutputTokenCount = 1000
-};
+        var requestOptions = new ChatCompletionOptions()
+        {
+            Temperature = 1.0f,
+            TopP = 1.0f,
+            MaxOutputTokenCount = 1000
+        };
 
-var response = client.CompleteChat(messages, requestOptions);
-System.Console.WriteLine(response.Value.Content[0].Text);
+        var response = client.CompleteChat(messages, requestOptions);
+        System.Console.WriteLine(response.Value.Content[0].Text);
     }
 }
